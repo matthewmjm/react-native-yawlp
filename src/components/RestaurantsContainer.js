8a9c2459576886e53d9296352c0fcebbaf4b26b3
matthewmjm <matthewmjm@yahoo.com>
2020-12-30 10:38:06 -0700

    get restaurant names to appear in the Restaurants Container

--- src/components/RestaurantsContainer.js
+++ src/components/RestaurantsContainer.js
@@ -1,8 +1,8 @@
 import React, { useEffect } from 'react';
 import { Text, View, StyleSheet } from 'react-native';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
-const apiKey = 
+const apiKey = ""
 
 const apiUrl = "https://api.yelp.com/v3/businesses/search?term=restaurant&location=Saint Louis";
 
@@ -18,11 +18,22 @@ export default function RestaurantsContainer() {
         .then(({businesses}) => dispatch({type: 'SET_RESTAURANTS', payload: businesses}))
     }, []);
 
+    const restaurants = useSelector(state => state.restaurants)
+    const showRestaurants = () => restaurants.map(restaurant => {
+        return <Text key={restaurant.id}>{restaurant.name}</Text>
+    })
+
     return (
-        <>
-            <Text>This is the Restaurant Container</Text>
-        </>
+        <View style={styles.container}>
+            <Text style={{ fontWeight: 'bold' }}>This is the Restaurant Container</Text>
+            {showRestaurants()}
+        </View>
     )
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+    }
+});
