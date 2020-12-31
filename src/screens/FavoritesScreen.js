6fd878de199f62eb57b44786b3c3b16e3220947d
matthewmjm <matthewmjm@yahoo.com>
2020-12-30 22:55:33 -0700

    render favorite restaurants on Favorites screen

--- src/screens/FavoritesScreen.js
+++ src/screens/FavoritesScreen.js
@@ -1,11 +1,28 @@
 import React from 'react'
-import { SafeAreaView, Text, StyleSheet, Platform, StatusBar } from 'react-native';
+import { SafeAreaView, Text, StyleSheet, Platform, StatusBar, ScrollView } from 'react-native';
+import { useSelector } from 'react-redux';
+import RestaurantCard from '../components/RestaurantCard';
 
 
-const FavoritesScreen = (props) => {
+const FavoritesScreen = () => {
+    const favorites = useSelector(state => state.favorites)
+
+    const displayFavorites = () => {
+        return favorites.map((favorite, i) => {
+            return <RestaurantCard 
+                key={favorite.id} 
+                restaurant={favorite}
+                index = {i + 1}
+            />
+        })
+    }
+
     return (
-        <SafeAreaView style={styles.container} >
+        <SafeAreaView style={styles.container}>
             <Text>FAVORITES SCREEN</Text>
+            <ScrollView style={styles.restaurantContainer}>
+                {displayFavorites()}
+            </ScrollView>
         </SafeAreaView>
     )
 }
@@ -17,6 +34,11 @@ const styles = StyleSheet.create({
         width: '100%',
         paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
     },
+    restaurantContainer: {
+        flex: 1,
+        margin: 15,
+    },
 });
+
 
 export default FavoritesScreen;
